cbcd520f8710929d268a3415272f483efbeb724a
Douglas Lockwood <doug@lockwood-family.com>
2023-01-21 15:37:22 -0700

    Changed output of 11ty to /docs instead of /_site (to work with Github Pages)

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: 'src',
       includes: '_includes',
-      output: '_site',
+      output: 'docs',
     },
     templateFormats: ['md', 'njk', 'html'],
     markdownTemplateEngine: 'njk',
